2f05d01ae43368dcd6d800a0a14f9053fbf3d20a
Barri <info@bdesignstudio.co.uk>
2020-05-21 17:27:04 +0100

    Three more test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,7 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
@@ -6,4 +10,14 @@ const app = shallow(<App />);
 
 it('renders correctly', () => {
   expect(app).toMatchSnapshot();
+});
+
+it('initializes the `state` with an empty list of gifts', ()=>{
+expect(app.state().gifts).toEqual([]);
+});
+
+it('Add new gifts to `state` when clicking `add gift` button', () =>{
+  app.find('.btn-add').simulate('click'); 
+
+  expect(app.state().gifts).toEqual([{ id: 1}]);
 });
